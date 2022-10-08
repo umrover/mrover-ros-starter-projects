effde5d1b5b427aeceab11b445fa96f652d54519
jpoly <jpoly@umich.edu>
2022-10-08 16:15:26 -0400

    initial

--- starter_project/autonomy/AutonomyStarterProject.cmake
+++ starter_project/autonomy/AutonomyStarterProject.cmake
@@ -8,6 +8,7 @@ add_message_files(
         DIRECTORY
         ${CMAKE_CURRENT_LIST_DIR}/msg
         FILES
+        StarterProjectTag.msg
 )
 
 # Collect all cpp files in the src subdirectory to be used for perception
