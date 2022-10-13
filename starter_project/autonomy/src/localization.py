8647bf37764177ebc7c5806820283ea8350b9a2f
jpoly <jpoly@umich.edu>
2022-10-12 23:16:30 -0400

    subscribing to sensor data

--- starter_project/autonomy/src/localization.py
+++ starter_project/autonomy/src/localization.py
@@ -20,6 +20,8 @@ class Localization:
     def __init__(self):
         # create subscribers for GPS and IMU data, linking them to our callback functions
         # TODO
+        rospy.Subscriber("/gps/fix", NavSatFix, self.gps_callback)
+        rospy.Subscriber("/imu", Imu, self.imu_callback)
 
         # create a transform broadcaster for publishing to the TF tree
         self.tf_broadcaster = tf2_ros.TransformBroadcaster()
@@ -35,6 +37,7 @@ class Localization:
         that pose to the TF tree.
         """
         # TODO
+        print(msg)
 
     def imu_callback(self, msg: Imu):
         """
@@ -43,6 +46,7 @@ class Localization:
         store that value in `self.pose`, then publish that pose to the TF tree.
         """
         # TODO
+        print(msg)
 
     @staticmethod
     def spherical_to_cartesian(spherical_coord: np.ndarray, reference_coord: np.ndarray) -> np.ndarray:
